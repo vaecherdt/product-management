84fcd55cf09cbb910b018a184b7804015102b6ad
vaecherdt <coup-aquatic-0e@icloud.com>
2024-11-02 10:09:41 -0300

    dynamic imports

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,3 @@
-import HomeView from '@/views/HomeView.vue'
-import ProjectsView from '@/views/ProjectsView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -8,12 +6,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'Home',
-      component: HomeView
+      component: () => import('@/views/HomeView.vue')
     },
     {
       path: '/projects',
       name: 'Projects',
-      component: ProjectsView
+      component: () => import('@/views/ProjectsView.vue')
     }
   ]
 })
