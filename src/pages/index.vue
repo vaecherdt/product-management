259d6b30eadc049ce5567135e4e7e782fffe0531
vaecherdt <coup-aquatic-0e@icloud.com>
2024-11-02 22:36:13 -0300

    refactor for file-based-routing

---
+++ src/pages/index.vue
@@ -0,0 +1,9 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <RouterLink :to="{ name: '/projects/[id]', params: {'id': 1} }">Project 1</RouterLink>
+  </div>
+</template>
